f4ee35d35420cdbcaf7b49c8c561818247babe49
Aymin Bashir <ayminbashir2001@gmail.com>
2023-08-09 03:17:00 -0700

    Added Home

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import logo from '../../assets/images/logo.png';
-import { links} from '../../assets/Data';
+import React from "react";
+import logo from "../../assets/images/logo.png";
+import { links } from "../../assets/Data";
 import { AiOutlineAppstore } from "react-icons/ai";
-import './Header.css';
+import "./Header.css";
 export default function Header() {
   return (
-    <header className='header'>
-      <div className='container'>
-      <nav className='nav-container'>
-        <a href="/" className='nav-logo'>
-          <img src={logo} alt="Logo" className='nav-img' />
-        </a>
-        <ul className='nav-menu'>
-          {links.map(({ name, path }, index) => (
-            <li className='nav-items' key={index}>
-              <a href={path} className='nav-link'>{name}</a>
-            </li>
-          ))}
-        </ul>
-       <div className='nav-toggle'>
-<AiOutlineAppstore />
-       </div>
-
-      </nav>
-      </div>
+    <header className="header">
+    
+        <nav className="nav-container container">
+          <a href="/" className="nav-logo">
+            <img src={logo} alt="Logo" className="nav-img" />
+          </a>
+          <ul className="nav-menu">
+            {links.map(({ name, path }, index) => (
+              <li className="nav-items" key={index}>
+                <a href={path} className="nav-link">
+                  {name}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className="nav-toggle">
+            <AiOutlineAppstore />
+          </div>
+        </nav>
+      
     </header>
   );
 }
---
+++ src/Components/Home/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Home.css'
+const Home = () => {
+    return (
+        <section className='home' id='home'>
+            <div className='home-container container'>
+                <div className='home-content'>
+                    <span className='home-subtitle'>
+                        Welcome To Coffero!
+                    </span>
+                    <h1 className='home-title'>
+                        Discover Amazing Coffe House &  Get Energy
+                    </h1>
+                    <p className='home-desc'>
+                        There are many variations of passages available but the majority have suffered alteration in some form y injected humour orrandomised words.
+                    </p>
+                    <div className='home-btns'>
+                        <a href="#menu" className='btn'>Check Menu</a>
+                        <a href="#reservation" className='btn book-btn'>Book Table</a>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    )
+}
+export default Home
