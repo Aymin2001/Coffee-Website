18c12417dd1b753494314c6af5cb5d4dea13d097
Aymin Bashir <ayminbashir2001@gmail.com>
2023-08-17 03:20:12 -0700

    added nav-toggle

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,11 @@
-import React from "react";
+import { React, useState } from "react";
 import logo from "../../assets/images/logo.png";
 import { links } from "../../assets/Data";
 import { Link, animateScroll } from "react-scroll";
 import { AiOutlineAppstore } from "react-icons/ai";
 import "./Header.css";
 export default function Header() {
+  const [showMenu, setshowMenu] = useState(false);
   const scrollTop = () => {
     animateScroll.scrollToTop();
   };
@@ -14,7 +15,7 @@ export default function Header() {
         <Link to="/" onClick={scrollTop} className="nav-logo">
           <img src={logo} alt="Logo" className="nav-img" />
         </Link>
-        <ul className="nav-menu">
+        <ul className={`nav-menu ${showMenu ? "show-menu" : ""}`}>
           {links.map(({ name, path }, index) => (
             <li className="nav-items" key={index}>
               <Link
@@ -24,13 +25,22 @@ export default function Header() {
                 duration={500}
                 to={path}
                 className="nav-link"
+                onClick={() => {
+                  setshowMenu(
+                    !showMenu
+                  )
+                }}
               >
                 {name}
               </Link>
             </li>
           ))}
         </ul>
-        <div className="nav-toggle">
+        <div className="nav-toggle" onClick={() => {
+          setshowMenu(
+            !showMenu
+          )
+        }}>
           <AiOutlineAppstore />
         </div>
       </nav>
